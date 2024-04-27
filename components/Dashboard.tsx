d3219c14038d3aed9280e8faf1f16121cb5c9384
smehra92 <mehrasiddharth1992@gmail.com>
2024-04-27 14:19:59 +0530

    Updated Code

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -6,7 +6,7 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 import React from 'react'
 
 const Dashboard = () => {
-    const { data: session } = useSession()
+    const { data: session } = useSession() //object destructuring to extract the data property from the object returned by the useSession() hook and assign it to a variable named session.
     return (
         <>
             {session ?
@@ -14,7 +14,7 @@ const Dashboard = () => {
                     <img src={session.user?.image as string} className=' rounded-full' />
                     <h1 className=' text-gray-400 text-2xl font-bold mt-5'>Welcome Back, {session.user?.name}</h1>
                     {session.user?.email}
-                    <button onClick={()=>{signOut({callbackUrl:`/dashboard`})}} className=' border border-black rounded-lg bg-red-300 py-1 px-5'>Sign out</button>
+                    <button onClick={()=>{signOut({callbackUrl:`/`})}} className=' border border-black rounded-lg bg-red-300 py-1 px-5'>Sign out</button>
 
                 </>
                 :
